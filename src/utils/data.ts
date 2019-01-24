6b01e7171dd14020977efb15c5fe94392e116548
Albert Alises <albertalises@Alberts-MacBook-Pro.local>
2019-01-24 22:56:41 +0100

    Change release date

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,6 +1,6 @@
 
 //Release Date
-export const releaseDate =  new Date(2019, 0 ,25,12);
+export const releaseDate =  new Date(2019, 0 ,25,0);
 
 export const MediaLinks = [
     {link:"https://www.instagram.com/urpaurpaurpa/" , icon: "fab fa-2x fa-instagram"},
